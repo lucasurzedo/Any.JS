5faca35e6439bc297a3c70f0b2aea28832f828c9
lucasurzedo <lucasurzedo1@gmail.com>
2021-05-10 15:50:44 -0300

    feat: change requests routes

--- src/express-server.js
+++ src/express-server.js
@@ -4,13 +4,14 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const mongoose = require('mongoose');
+require('dotenv/config')
 
 exports.setupApp = async function setupApp() {
 	app.use(express.json());
 
-	app.use('/api/anyJS', routes);
+	app.use('/api/anyJS/v1', routes);
 
-	mongoose.connect('mongodb://db:27017/anyjs-db', { useNewUrlParser: true, useUnifiedTopology: true }, () => 
+	mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => 
 		console.log('Connected to DB!')
 	);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,35 @@
 
 const express = require('express');
 const router = express.Router();
-const { instantiateStoreController, instantiateAccessController, taskCreate, taskExecute, getAllTasks, getAllTaskExecutions, getATask, getAExecution, deleteATask, deleteAExecution, observerExecution } = require('../controllers');
+const controllers = require('../controllers');
 
-router.post('/instantiate/store', instantiateStoreController);
-router.post('/instantiate/access', instantiateAccessController);
-router.post('/execute/task', taskCreate);
-router.post('/execute/task/:taskName/execution', taskExecute);
-router.get('/execute/task', getAllTasks);
-router.get('/execute/task/:taskName/execution', getAllTaskExecutions);
-router.get('/execute/task/:taskName', getATask);
-router.get('/execute/task/:taskName/execution/:executionName', getAExecution);
-router.delete('/execute/task/:taskName', deleteATask);
-router.delete('/execute/task/:taskName/execution/:executionName', deleteAExecution);
+// requests for register service
+router.post('/register', controllers. registerCode);
+router.get('/register/:codeName', controllers.getCode);
+router.delete('/register/:codeName', controllers.deleteCode);
+
+// requests for store service
+router.post('/store/object', controllers.createObject);
+router.get('/store/object/', controllers.getAllObjects);
+router.get('/store/object/:objectName', controllers.getObject);
+router.delete('/store/object/:objectName', controllers.deleteObject);
+
+// requests for instantiate service
+
+// requests for execute service
+router.post('/execute/task', controllers.createTask);
+router.post('/execute/task/:taskName/execution', controllers.taskExecute);
+router.get('/execute/task', controllers.getAllTasks);
+router.get('/execute/task/:taskName/execution', controllers.getAllTaskExecutions);
+router.get('/execute/task/:taskName', controllers.getTask);
+router.get('/execute/task/:taskName/execution/:executionName', controllers.getExecution);
+router.delete('/execute/task/:taskName', controllers.deleteTask);
+router.delete('/execute/task/:taskName/execution/:executionName', controllers.deleteExecution);
+
+// requests for sync service
+
+// requests for lock and unlock service
+
+
 
 module.exports = router;
