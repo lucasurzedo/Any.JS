e6daf3039aa6e61344eb1aeb3b87d8aaac336759
lucasurzedo <lucasurzedo1@gmail.com>
2021-10-13 15:08:24 -0300

    fix: remove unused function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,81 +1,4 @@
-const mongoose = require('mongoose');
-const pluralize = require('pluralize');
 const Downloader = require('nodejs-file-downloader');
-const ModelTask = require('../models/task');
-const executeFunction = require('../services/executeFunction');
-
-function createExecution(execution, res, jsonResult, taskName, multipleExecutions) {
-  const collectionName = taskName.toLowerCase();
-  const parameterValueResult = JSON.parse(JSON.stringify(execution.parameterValue));
-
-  mongoose.connection.db.collection(pluralize(collectionName), (err, collection) => {
-    if (err) {
-      console.log(err);
-    }
-
-    // eslint-disable-next-line consistent-return
-    collection.findOne({ taskName }, async (error, data) => {
-      if (error) {
-        console.log(error);
-      } else if (data === null) {
-        try {
-          jsonResult.result = `Cannot find ${collectionName} task`;
-          jsonResult.status = 404;
-          return res.send(jsonResult);
-        } catch (erro) {
-          console.log(erro);
-        }
-      } else {
-        const execName = execution.executionName;
-        const findExecutionName = await collection.findOne({ executionName: execName });
-        if (findExecutionName != null) {
-          try {
-            jsonResult.result = `Execution ${execution.executionName} already exist`;
-            jsonResult.status = 409;
-            return res.send(jsonResult);
-          } catch (erro) {
-            console.log(erro);
-          }
-        } else {
-          try {
-            const Task = mongoose.model(taskName, ModelTask);
-            const newTask = new Task({
-              executionName: execution.executionName,
-              parameterValue: parameterValueResult,
-              taskResult: null,
-            });
-            try {
-              await newTask.save();
-              jsonResult.result = 'saving execution';
-              jsonResult.status = 201;
-
-              if (!multipleExecutions) {
-                res.send(jsonResult);
-              }
-
-              const params = execution.parameterValue;
-              executeFunction({ parameterValue: params, jsonData: data }).then((result) => {
-                newTask.taskResult = result;
-                newTask.save();
-              });
-            } catch (erro) {
-              // Retornar um json de erro
-              console.log('Header sent to the client');
-            }
-          } catch (erro) {
-            try {
-              jsonResult.error = 'server error';
-              jsonResult.status = 404;
-              return res.send(jsonResult);
-            } catch (serverErr) {
-              console.log(serverErr);
-            }
-          }
-        }
-      }
-    });
-  });
-}
 
 async function downloadCode(methodsLinks) {
   console.log(methodsLinks);
@@ -105,7 +28,6 @@ function validVariable(input) {
 }
 
 module.exports = {
-  createExecution,
   downloadCode,
   validVariable,
 };
