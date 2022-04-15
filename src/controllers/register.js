f69198ea9e8482d56acf32d056b3d842b53c5415
lucasurzedo <lucasurzedo1@gmail.com>
2022-04-14 23:10:47 -0300

    refactor(register): refactor getCode to db functions

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -130,7 +130,7 @@ async function updateCodeElement(req, res) {
 }
 
 async function updateCode(req, res) {
-  if (!req.body.codeName || !req.body.code) {
+  if (!req.body.code) {
     const jsonError = {
       uri: `${req.baseUrl}${req.url}`,
       result: 'invalid JSON',
@@ -143,40 +143,52 @@ async function updateCode(req, res) {
   }
 
   const newValues = {
-    $set: { code: req.body.code, value: req.body.value },
+    $set: { code: req.body.code},
     $currentDate: { lastModified: true },
   };
 
+  const query = {};
+  query.codeName = req.params.codeName;
+
+  const updated = await db.updateDocument(collectionName, query, newValues);
+
+  if (updated.modifiedCount > 0) {
+    const jsonResult = {
+      uri: `${req.baseUrl}${req.url}`,
+      status: 200,
+    };
+
+    res.send(jsonResult);
+  } else {
+    const jsonError = {
+      uri: `${req.baseUrl}${req.url}`,
+      result: `there is no code ${req.body.key}`,
+      status: 404,
+    };
+
+    res.send(jsonError);
+  }
 }
 
 async function getCode(req, res) {
-  mongoose.connection.db.collection(collectionName, (error, collection) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
+  const document = await db.getDocument(collectionName, 'codeName', req.params.codeName);
 
-    collection.findOne({ codeName: req.params.codeName }, async (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      if (!data) {
-        const jsonError = {
-          uri: `${req.baseUrl}${req.url}`,
-          result: `cannot find code ${req.params.codeName}`,
-          status: 406,
-        };
-        res.send(jsonError);
-      } else {
-        const jsonResult = {
-          uri: `${req.baseUrl}${req.url}`,
-          object: data,
-          status: 200,
-        };
-        res.send(jsonResult);
-      }
-    });
-  });
+  if (document) {
+    const jsonResult = {
+      uri: `${req.baseUrl}${req.url}`,
+      result: document,
+      status: 200,
+    };
+
+    res.send(jsonResult);
+  } else {
+    const jsonError = {
+      uri: `${req.baseUrl}${req.url}`,
+      result: `there is no code ${req.params.codeName}`,
+      status: 404,
+    };
+    res.send(jsonError);
+  }
 }
 
 async function deleteCode(req, res) {
