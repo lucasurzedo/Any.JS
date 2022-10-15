a50ab9ad4648a1eea753819453cf6ba6f0b59805
lucasurzedo <lucasurzedo1@gmail.com>
2022-10-15 16:23:51 -0300

    fix(db): update mongoose connection

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ async function setupApp() {
   app.use(express.json());
   app.use('/api/anyJS/v1', routes);
 
-  mongoose.connect('mongodb://db:27017/anyjs-db', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to DB!'));
+  mongoose.connect('mongodb://mongo:27017/anyjs', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to DB!'));
 
   return app;
 }
